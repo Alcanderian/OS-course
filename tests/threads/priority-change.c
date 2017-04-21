94146346cbe9095483d1dcd8f6a8c8f6dba21042
Alcanderian <alcanderian@gmail.com>
2017-04-21 17:12:00 +0800

    add comments

--- tests/threads/priority-change.c
+++ tests/threads/priority-change.c
@@ -15,22 +15,28 @@ test_priority_change (void)
   /* This test does not work with the MLFQS. */
   ASSERT (!thread_mlfqs);
 
+  /* About `putchar('%d')`:
+   *   Comment all `msg` and uncomment `putchar`, then we can see the actual
+   *   result of the test. Because msg runs too slow that will impact the
+   *   result. For example, in ubuntu-14.04 and ubuntu-16.04, we will see
+   *   different result if we only change `threads_ser_priority`.
+   */
   msg ("Creating a high-priority thread 2.");
-  /* putchar('1'); */
+  /* putchar ('1'); */
   thread_create ("thread 2", PRI_DEFAULT + 1, changing_thread, NULL);
   msg ("Thread 2 should have just lowered its priority.");
-  /* putchar('3'); */
+  /* putchar ('3'); */
   thread_set_priority (PRI_DEFAULT - 2);
   msg ("Thread 2 should have just exited.");
-  /* putchar('5'); */
+  /* putchar ('5'); */
 }
 
 static void
 changing_thread (void *aux UNUSED)
 {
   msg ("Thread 2 now lowering priority.");
-  /* putchar('2'); */
+  /* putchar ('2'); */
   thread_set_priority (PRI_DEFAULT - 1);
   msg ("Thread 2 exiting.");
-  /* putchar('4'); */
+  /* putchar ('4'); */
 }
