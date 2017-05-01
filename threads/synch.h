e5e5a41787375dc78ec744219fd53b0c29a42f7a
Alcanderian <alcanderian@gmail.com>
2017-05-02 02:16:15 +0800

    add lock member

--- threads/synch.h
+++ threads/synch.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 /* A counting semaphore. */
-struct semaphore 
+struct semaphore
   {
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
@@ -18,11 +18,20 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock 
+struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+
+    /* By alcanderian */
+    struct list_elem hook;      /* Hook in holder lock list. */
+    int priority;               /* Priority of lock. */
+    /* By alcanderian */
   };
+
+/* By alcanderian */
+#define lock_entry(LIST_ELEM) (list_entry (LIST_ELEM, struct lock, hook))
+/* By alcanderian */
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
@@ -31,7 +40,7 @@ void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
 /* Condition variable. */
-struct condition 
+struct condition
   {
     struct list waiters;        /* List of waiting threads. */
   };
