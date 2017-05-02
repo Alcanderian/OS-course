585997bf5702c72d4c036953182e6d29d61fe71a
Alcanderian <alcanderian@gmail.com>
2017-05-03 03:50:15 +0800

    pass all donate test. add comment.

--- threads/synch.c
+++ threads/synch.c
@@ -117,8 +117,8 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters))
     {
-      /* Using LIST_SORT maybe more useful, because
-         thread's priority maybe change. */
+      /* Using LIST_SORT is comfirmed more useful, because
+         thread's priority maybe change among sema operations. */
       list_sort (&sema->waiters, thread_compare_by_priority, NULL);
       thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                   struct thread, elem));
