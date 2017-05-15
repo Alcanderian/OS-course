5636a3f505ba5a0212e4b0a3d69ce62acd17b3f9
Alcanderian <alcanderian@gmail.com>
2017-05-16 04:59:44 +0800

    fix

--- threads/thread.c
+++ threads/thread.c
@@ -290,7 +290,7 @@ thread_update_priority (struct thread *t, void *aux UNUSED)
         return;
 
       fixed_t inv_priority = fp_addi (fp_divi (t->cpu, 4), (2 * t->nice));
-      t->priority = PRI_MAX - inv_priority;
+      t->priority = PRI_MAX - fp_round (inv_priority);
       if(t->priority < PRI_MIN)
         t->priority = PRI_MIN;
     }
