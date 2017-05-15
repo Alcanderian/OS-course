403706cb744f5f1e55abe8ef427f652fdc05f3d2
Alcanderian <alcanderian@gmail.com>
2017-05-16 05:12:00 +0800

    why?

--- threads/thread.c
+++ threads/thread.c
@@ -292,9 +292,6 @@ thread_update_priority (struct thread *t, void *aux UNUSED)
 
       inv_priority = fp2i (fp_addi (t->cpu >> 2, t->nice << 1));
       t->priority = PRI_MAX - inv_priority;
-
-      if(t->priority < PRI_MIN)
-        t->priority = PRI_MIN;
     }
 }
 
