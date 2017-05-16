112eff325c5e27de13850fb13ade8412c563168e
Alcanderian <alcanderian@gmail.com>
2017-05-16 12:46:00 +0800

    pass all 27

--- threads/synch.c
+++ threads/synch.c
@@ -33,6 +33,9 @@
 #include "threads/thread.h"
 #include "lib/algori.h"
 
+bool sema_elem_great_priority (const struct list_elem *a,
+                               const struct list_elem *b, void *aux UNUSED);
+
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
    nonnegative integer along with two atomic operators for
    manipulating it:
